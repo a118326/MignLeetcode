2a6b4bc6116724d142586aa7502d252f8282b207
a118326 <mchen2015@fau.edu>
2019-04-10 23:34:39 -0400

    04/10/2019

---
+++ AlgorithmQs/627_Swap_Salary.sql
@@ -0,0 +1,9 @@
+--627. Swap Salary
+--Easy
+
+--SQL Schema
+--Given a table salary, such as the one below, that has m=male and f=female values.
+--Swap all f and m values (i.e., change all f values to m and vice versa) with a single update statement and no intermediate temp table.
+--Note that you must write a single update statement, DO NOT write any select statement for this problem.
+
+update salary set sex=if(sex='m','f','m')
